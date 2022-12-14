3bba588ff331150b378728a91346bddc82bbbcb2
Dadepo Aderemi <dadepo@gmail.com>
2022-12-14 21:24:18 +0100

    grouping into success/failure early

--- src/processor.rs
+++ src/processor.rs
@@ -3,33 +3,39 @@ use futures::StreamExt;
 use crate::client::{FailurePayload, GroupedResult, ResponsePayload, SuccessPayload};
 
 
-pub fn group_success_failure(raw_result: Vec<ResponsePayload>) -> GroupedResult {
-    let (successes, failures): (Vec<ResponsePayload>, Vec<ResponsePayload>) = raw_result.into_iter().partition(|raw| {
-        raw.payload.is_ok()
+pub fn group_success_failure(responsePayload: Vec<ResponsePayload>) -> GroupedResult {
+    let (successes, failures): (Vec<SuccessPayload>, Vec<FailurePayload>) = responsePayload
+        .into_iter()
+        .fold((vec![], vec![]), |mut acc, result| {
+        match result.payload {
+            Ok(success) => {
+                acc.0.push(SuccessPayload {
+                    payload: success,
+                    endpoint: result.endpoint
+                });
+                acc
+            },
+            Err(err) => {
+                acc.1.push(FailurePayload {
+                    payload: err,
+                    endpoint: result.endpoint
+                });
+                acc
+            }
+        }
     });
 
-    let mut hash_to_value_map: HashMap<String, Vec<SuccessPayload>> = HashMap::new();
+    let mut hash_to_successes: HashMap<String, Vec<SuccessPayload>> = HashMap::new();
     successes.into_iter().for_each(|entry| {
-        hash_to_value_map
-            .entry(entry.payload.as_ref().unwrap().data.finalized.root.clone())
+        hash_to_successes
+            .entry(entry.payload.data.finalized.root.clone())
             .or_default()
-            .push(SuccessPayload {
-                payload: entry.payload.unwrap(),
-                endpoint: entry.endpoint,
-            })
+            .push(entry)
     });
 
 
     GroupedResult {
-        success: hash_to_value_map,
-        failure: failures.into_iter().map(|failure| {
-            match failure.payload {
-                Ok(_) => panic!("Unexpected success processed as failure"),
-                Err(e) => FailurePayload {
-                    payload: e,
-                    endpoint: failure.endpoint
-                }
-            }
-        }).collect()
+        success: hash_to_successes,
+        failure: failures
     }
 }
